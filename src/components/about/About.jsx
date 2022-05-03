c98802af87076db51aeaa3f7ef7ad1e4b668f0a2
Alan Trapenard <atrapena@ucsd.edu>
2022-05-03 14:44:11 -0700

    Corrected Nav Bar issue with Experience section Added sections and article elements for creation of experience, clients, and projects Smooth scroll now functional

---
+++ src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './about.css'
+import ME from '../../assets/me.jpg.png'
+import {RiAwardFill} from 'react-icons/ri'
+
+const About = () => {
+  return (
+    <section id='about'>
+      <h5>Get To Know Me</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className='about__card'>
+              <RiAwardFill className='about__icon'/>
+              <h5>Experience</h5>
+              <small>3+ Years Working</small>
+            </article>
+            <article className='about__card'>
+              
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+import {DiGithubFull} from 'react-icons/di'
+import {FaInstagramSquare} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'>
+        <a href="https://www.linkedin.com/in/alan-trapenard-790670199/" target='_blank'><BsLinkedin /></a>
+        <a href="https://github.com/ATrapenard" target='_blank'><DiGithubFull /></a>
+        <a href="https://instagram.com/alan.trapezoid/" target='blank'><FaInstagramSquare /></a>
+    </div>
+  )
+}
+
+export default HeaderSocials
