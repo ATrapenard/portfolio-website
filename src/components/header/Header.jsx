240e91e279591494b49b3520c82dd406af776b1b
Alan Trapenard <alan.trapenard@gmail.com>
2022-05-25 18:26:11 -0700

    changed styling of header component and added bio divs as well as a short bio

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,10 +12,19 @@ const Header = () => {
         <h1>Alan Trapenard</h1>
         <h5 className="text-light">Frontend Developer</h5>
         <CTA />
-        <HeaderSocials />
-        <div className="me">
-          <img src={ME} alt="me" />
+        <div className="header__bio">
+          <div className="me">
+            <img src={ME} alt="me" />
+          </div>
+          <div className="header__bio-content">
+            <h1>
+              I'm a San Diego based aspiring Software Engineer working primarily
+              with JavaScript/React to design interactive web based
+              applications.
+            </h1>
+          </div>
         </div>
+        <HeaderSocials />
         <a href="#contact" className="scroll__down">
           Scroll Down
         </a>
